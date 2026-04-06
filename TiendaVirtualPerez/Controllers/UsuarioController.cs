b6b071bc40c63729e9815a22371227b1fd1398e6
CamiloPerez07 <juancamiloperez175@gmail.com>
2026-04-06 14:00:36 -0500

    Users Create, Edit and Delete

---
+++ TiendaVirtualPerez/Controllers/UsuarioController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendaVirtualPerez.Data;
+using TiendaVirtualPerez.Models;
+
+namespace TiendaVirtualPerez.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly TiendaContext _context;
+
+        public UsuarioController(TiendaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var usuarios = _context.Usuarios
+                .ToList();
+
+            return View(usuarios);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Usuario usuario)
+        {
+            _context.Usuarios.Add(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Usuario usuario)
+        {
+            _context.Usuarios.Update(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int id)
+        {
+            var usuario = _context.Usuarios.Find(id);
+
+            _context.Usuarios.Remove(usuario);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
