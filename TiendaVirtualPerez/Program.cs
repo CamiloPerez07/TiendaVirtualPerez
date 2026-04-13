fcb3800eb6f531ce43463d536b3eb30e7dfed060
luis gabriel <lglopez.8956@unicesmag.edu.co>
2026-04-13 17:35:50 -0500

    Configure Login and protect producto and usuario

--- TiendaVirtualPerez/Program.cs
+++ TiendaVirtualPerez/Program.cs
@@ -3,9 +3,10 @@ using TiendaVirtualPerez.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSession();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+    
 
 builder.Services.AddDbContext<TiendaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("TiendaContextConnection")));
@@ -19,7 +20,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+app.UseSession();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
